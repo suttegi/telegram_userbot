e9119322f602c8061814a9a31a941b6bd4bcf75e
synapse <60628137+uipoetry@users.noreply.github.com>
2023-11-13 03:07:19 +0600

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ exec(config)
 
 app = Client("my_bot", api_id=api_id, api_hash=api_hash)
 
-# client = OpenAI(api_key=openai.api_key)
+
 
 
 
@@ -29,25 +29,6 @@ def handle_message(client, message):
     bot_reply = response["choices"][0]["message"]["content"]
     message.reply(bot_reply)
 
-# @app.on_message(filters.command("image"))
-# def handle_image_generation(client, message):
-#     # Получаем текст после команды "/image"
-#     user_input = " ".join(message.command[1:])
-
-#     # Генерируем изображение только при наличии текста
-#     if user_input:
-#         prompt = f'{user_input}'
-#         image_response = openai.Image.create(
-#             model=os.environ.get('OPENAI_MODEL', 'image-alpha-001'),
-#             prompt=prompt,
-#             max_tokens=50,
-#             temperature=1.0
-#         )
-#         image_url = response.data[0].url
-#         # Отправляем изображение пользователю
-#         app.send_photo(message.chat.id, photo=image_url)
-#     else:
-#         app.send_message(message.chat.id, "Пожалуйста, укажите текст после команды /image.")
 
 
 if __name__ == "__main__":
